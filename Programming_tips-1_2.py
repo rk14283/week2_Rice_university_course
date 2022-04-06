5aeca419b93e58085ba87d4796efde253315d238
rk14283 <59180436+rk14283@users.noreply.github.com>
2022-04-06 22:09:49 +0530

    Create Programming_tips-1_2.py

---
+++ Programming_tips-1_2.py
@@ -0,0 +1,12 @@
+#name error
+#fixed after importing random
+
+import random
+def random_dice():
+    die1 = random.randrange(1,7)
+    die2 = random.randrange(1,7)
+    return die1 + die2
+
+print "Sum of two random dice, rolled once:", random_dice()
+print "Sum of two random dice, rolled again:", random_dice()
+print "Sum of two random dice, rolled again", random_dice 
