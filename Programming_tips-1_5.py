76456d1b5f2a9f222a5f4416d1d97862ee54cbce
rk14283 <59180436+rk14283@users.noreply.github.com>
2022-04-06 22:58:27 +0530

    Update Programming_tips-1_5.py

--- Programming_tips-1_5.py
+++ Programming_tips-1_5.py
@@ -1,15 +1,15 @@
 import math 
 
-def area_triangle_sss(side1,side1,side3):
+def area_triangle_sss(side1, side2, side3):
     """Returns the area of a triangle given the lenghts
     of its three sides
     """
     #Heron's formula 
-    semi_perim = (side1+side2+side3)/2.0
+    semi_perim = (side1 + side2+ side3) / 2.0
     return math.sqrt(semi_perim * 
-                    (semi_perim-a) *
-                    (semi_perim-b) *
-                     (semi_perim-c))
+                    (semi_perim- side1) *
+                    (semi_perim- side2) *
+                    (semi_perim- side3))
 
 
 
@@ -20,5 +20,4 @@ it allows the string to over over multiple lines
  but not how it does it"""
 
 
-
-
+#no output 
