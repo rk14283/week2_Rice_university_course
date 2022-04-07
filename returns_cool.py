3d003d576788fe15699504e18bc69bb8505526f4
rk14283 <59180436+rk14283@users.noreply.github.com>
2022-04-07 11:02:34 +0530

    Create returns_cool.py

---
+++ returns_cool.py
@@ -0,0 +1,43 @@
+# Compute whether a person is cool.
+
+###################################################
+# Is cool formula
+# Student should enter function on the next lines.
+
+def is_cool(name):
+    """Returns cool kids"""
+    if(name)== "Joe":
+        return True
+    elif (name) == "John":
+        return True 
+    elif (name) == "Stephen": 
+        return True
+    else:
+        return False 
+
+
+###################################################
+# Tests
+# Student should not change this code.
+
+def test(name):
+    """Tests the is_cool function."""
+    
+    if is_cool(name):
+        print name, "is cool."
+    else:
+        print name, "is not cool."
+
+test("Joe")
+test("John")
+test("Stephen")
+test("Scott")
+
+###################################################
+# Expected output
+# Student should look at the following comments and compare to printed output.
+
+#Joe is cool.
+#John is cool.
+#Stephen is cool.
+#Scott is not cool.
